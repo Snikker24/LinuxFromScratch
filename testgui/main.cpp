bf5dc53bc33e82c4d34d559767ef7c53791ccc0c
David <48534286+Snikker24@users.noreply.github.com>
2025-06-23 23:05:03 +0300

    Add files via upload

--- testgui/main.cpp
+++ testgui/main.cpp
@@ -2,6 +2,25 @@
 
 using namespace std;
 
+
+int index(int a, int b, std::string msg){
+
+    if(a>b)
+        swap(a,b);
+
+    int idx;
+    do{
+
+        std::cout<<msg;
+        std::cin>>idx;
+
+    }while(idx<a||idx>=b);
+
+
+    return idx;
+
+}
+
 int main()
 {
 
@@ -13,14 +32,29 @@ int main()
 
     }
 
-    int idx;
-    do{
-
-        std::cout<<"Device index:";
-        std::cin>>idx;
-
-    }while(idx<0||idx>=devices.size());
+    int idx=idx(0,devices.size(),"Device index: ")
 
 
+    DrmDevice device=devices[idx];
+    std::vector<DrmDevice::DrmConnector> connectors=device.connectors();
+    std::cout<<"Choose DRM device connector:\n";
+    for(int i=0;i<connectors.size();i++){
+
+        std::cout<<"["<<i<<"]: connector-id="<<connectors[i].id()<<"\n";
+
+    }
+
+    idx=idx(0,connectors.size(),"Connector index: ")
+
+    DrmDevice::DrmConnector connector=connectors[idx];
+    std::vector<DrmDevice::DrmConnector::DisplayMode> modes=connector.modes();
+    std::cout<<"Choose DRM connector mode:\n";
+    for(int i=0;i<connectors.size();i++){
+
+        std::cout<<"["<<i<<"]: "<<modes[i].width()<<"x"<<modes[i].height()<<"@"<<modes[i].frequency()<<<<"\n";
+
+    }
+
+    int idx=idx(0,modes.size(),"Mode index: ")
 
 }
